06851eec6dc459f4cad68ad96c9c971570d89c9b
rayelisson <33360322+Rayelisson@users.noreply.github.com>
2023-11-08 20:21:46 -0300

    parte um no projeto no dia 08/11/23

---
+++ api/src/app.module.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+
+
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),ProductModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
